82b6a911e8c96a7633f02a197ed4e90df3415777
Anatoly <0711975@gmail.com>
2021-01-22 23:12:53 +0300

    Task_15 -  итерация по ключу переданного объекта. Описание в README.md. # Задача 1

--- iterate.js
+++ iterate.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const obj1 = {name: 'James', age: 45, height: 179, weight: 71};
+const obj = {a: 1, b: 2, c:3};                                      // объявлен объект со свойствами
+
+const iterate = (object, callback) => {                             // объявлена функция с аргументами, принимающими объект и функцию
+    callback();                                                     // вызов функции, которая становится аргументом ф-ии iterate();
+    console.dir(object);                                            // вывод свойств объекта в консоль
+}
+
+const showObject = function(key) {                                  // объявлена ф-ия, итерирующая ключи переданного объекта
+    for (key in obj) {
+        console.log(`{ key: '${key}', value: ${obj[key]} }`);       // вывод в консоль ключа и знаяения свойства объекта
+    }
+}
+
+obj.d = 4;                                                          // добавлено свойство объекта obj с ключем d и присвоением ему значения 4
+iterate(obj, showObject);                                           // вызов ф-ии iterate()
