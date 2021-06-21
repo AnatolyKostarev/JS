8feb180c6b9a9aa882fdbd36e17f3ed2cffc6a58
Anatoly <0711975@gmail.com>
2021-06-21 15:33:45 +0300

    Slider - внесены изменения в обработчик вывода картинок в слайдер. Добавлены обработчики событий мыши для вывода миниатюры картинки при наведении мыши на кнопку сдайдера

--- slider.js
+++ slider.js
@@ -2,12 +2,13 @@
 
 const output = document.getElementById('output');
 const buttons = document.querySelectorAll('nav button');
+const preview = document.getElementById('preview');
 
 let current = buttons[0];
 
 function showImage() {
     current = this;
-    output.src = 'img/' + this.id + '.jpg';
+    output.src = getImageURL(this.id);
     buttons.forEach((el) => {
         if (el == this) {
             el.className = 'navButtons active';
@@ -21,5 +22,22 @@ function showImage() {
 buttons.forEach(
     (el) => {
         el.addEventListener('click', showImage);
+        el.addEventListener('mouseover', showPreview);
+        el.addEventListener('mouseout', hidePreview);
     }
 );
+
+function getImageURL(fileName) {
+    return 'img/' + fileName + '.jpg'
+}
+
+function showPreview(evt) {
+    preview.style.left = evt.pageX + 'px';
+    preview.style.top - evt.pageY + 'px';
+    preview.style.backgroundImage = 'url(' + getImageURL(this.id) + ')';
+    preview.className = 'active';
+}
+
+function hidePreview() {
+    preview.className = '';
+}
