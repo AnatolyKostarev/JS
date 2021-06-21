1c6a450efbf0d3b000113bb6257397dc6d26eb40
Anatoly <0711975@gmail.com>
2021-06-21 16:21:04 +0300

    Slider - technical commit

--- slider.js
+++ slider.js
@@ -3,6 +3,7 @@
 const output = document.getElementById('output');
 const buttons = document.querySelectorAll('nav button');
 const preview = document.getElementById('preview');
+const buttonset = document.getElementById('buttonset');
 
 let current = buttons[0];
 
@@ -14,7 +15,7 @@ function showImage() {
             el.className = 'navButtons active';
         }
         else {
-            el.className = 'navButtons';
+             el.className = 'navButtons';
         }        
     });
 }
